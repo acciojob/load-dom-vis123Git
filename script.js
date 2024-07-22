7dd2b20f746d8ef36cff8fe7d838afa35ddd4f09
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-07-22 09:30:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
         document.addEventListener('DOMContentLoaded', function() {
-            const message = document.createElement('p');
-            message.textContent = "DOM load success";
-            document.body.appendChild(message);
+                       document.write("DOM load success");
+
         });
